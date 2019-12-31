eb66886e836f91afdb8f6eeb49fa4d097822580f
Ben Sommervlle <Reverie@Juice.local>
2019-12-31 13:29:34 -0500

    first commit. AA and devise setup

---
+++ app/models/community.rb
@@ -0,0 +1,6 @@
+class Community < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
